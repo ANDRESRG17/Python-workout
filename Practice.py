532a9a487d9c33f8a06357cbb252cd85e9d0eb69
iam17 <aarincong@gmail.com>
2022-03-09 22:15:45 -0500

    Exercise No. 1 finished

--- Practice.py
+++ Practice.py
@@ -5,19 +5,20 @@ import random
 def guessing_game():
     
     n = random.randint(0,100)
-    
+    p = 0
+    print(n)
     while p!=n:
         
         p = int(input(print("Digita el número que crees")))
         
         if p>n:
             
-            print("Too low")
+            print("Too high")
             
         elif p<n:
             
-            print('Too high')
+            print('Too low')
     
-    return (n,p)
+    return f'Adivinaste que el número es: {n}'
 
 guessing_game()
