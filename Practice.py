d1ac2c3db298cefa7893b2425f713729871f8c6f
iam17 <aarincong@gmail.com>
2022-03-09 20:09:33 -0500

    Creación de función

---
+++ Practice.py
@@ -0,0 +1,24 @@
+## Exercise No. 1: Number guessing name
+
+import random
+
+def guessing_game():
+    
+    n = random.randint(0,100)
+    p = 200
+    
+    while p!=n:
+        
+        p = int(input(print("Digita el número que crees")))
+        
+        if p>n:
+            
+            print("Too low")
+            
+        elif p<n:
+            
+            print('Too high')
+    
+    return (n,p)
+
+guessing_game()
